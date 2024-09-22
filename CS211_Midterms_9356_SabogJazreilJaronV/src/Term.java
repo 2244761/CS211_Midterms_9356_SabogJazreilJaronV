f602a488712bb1b9b2f75c17dd046b1e8c85611b
Atlas-jjs <sabogjazreiljaron@gmail.com>
2024-09-22 14:51:28 +0800

    Created the Term Reference Class

--- CS211_Midterms_9356_SabogJazreilJaronV/src/Term.java
+++ CS211_Midterms_9356_SabogJazreilJaronV/src/Term.java
@@ -1,2 +1,44 @@
-public class Term {
+public class Term implements Comparable<Term> {
+    private float coefficient;
+    private int degree;
+
+    public Term() {
+        coefficient = 0.0f;
+        degree = 0;
+    }
+
+    public Term(float coefficient, int degree) {
+        this.coefficient = coefficient;
+        this.degree = degree;
+    }
+
+    public float getCoefficient() {
+        return coefficient;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public void setCoefficient(float coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+
+    public String toString() {
+        return "Coefficient: " + coefficient + "\nDegree: " + degree;
+    }
+
+    @Override
+    public int compareTo(Term another) {
+        // Compare the degrees first
+        if (this.getDegree() > another.getDegree()) return -1;
+        else if (this.getDegree() < another.getDegree()) return 1;
+
+        // If degrees are equal, compare coefficients
+        return Float.compare(this.getCoefficient(), another.getCoefficient());
+    }
 }
