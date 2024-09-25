a51e15635e2a0d107c9d4c9d4e8830ca09bd9f4c
Atlas-jjs <sabogjazreiljaron@gmail.com>
2024-09-25 22:53:14 +0800

    Finished the formula for polynomialAddition method and finalized the printPolynomial method

--- CS211_Midterms_9356_SabogJazreilJaronV/src/PolynomialArithmetic.java
+++ CS211_Midterms_9356_SabogJazreilJaronV/src/PolynomialArithmetic.java
@@ -5,11 +5,20 @@ import java.util.Scanner;
 public class PolynomialArithmetic {
     private final Scanner input = new Scanner(System.in);
     public void polynomialEvaluation() {
-        System.out.println("POLYNOMIAL EVALUATION");
         LinkedList<Term> Polynomial = construct();
 
-        System.out.print("Value to be assigned to the literals of the polynomial: ");
-        byte value = Byte.parseByte(input.nextLine());
+        System.out.print("Value to be assigned to the Literals of the Polynomial: ");
+        byte value;
+
+        while (true) {
+            try {
+                 value = Byte.parseByte(input.nextLine());
+                break;
+            } catch (NumberFormatException | InputMismatchException e) {
+                System.out.print("Enter a valid input: ");
+            }
+        }
+
         float total = 0;
 
         for (Term term : Polynomial) {
@@ -20,37 +29,42 @@ public class PolynomialArithmetic {
             }
         }
         System.out.println("The polynomial evaluates to : " + total);
+        System.out.print("Press ENTER to continue...");
+        input.nextLine();
+        System.out.println();
     }
 
     public void polynomialAddition() {
         System.out.println("POLYNOMIAL ADDITION");
-        System.out.println("First Polynomial: ");
-        LinkedList<Term> firstPoly = construct();
-        System.out.println("Second Polynomial: ");
-        LinkedList<Term> secondPoly = construct();
+        System.out.println("FIRST POLYNOMIAL");
+        LinkedList<Term> firstPolynomial = construct();
+        System.out.println("SECOND POLYNOMIAL");
+        LinkedList<Term> secondPolynomial = construct();
 
-        LinkedList<Term> finalPoly = new LinkedList<>();
+        LinkedList<Term> sumPolynomial = new LinkedList<>();
 
-        int degrees = Math.max(firstPoly.getFirst().getDegree(), secondPoly.getFirst().getDegree());
-        int less = Math.min(firstPoly.getFirst().getDegree(), secondPoly.getFirst().getDegree());
-
-
-        for(int i = degrees; i >= 0; i--) {
-            for (int j = less; j >= 0; j--) {
-                if (firstPoly.get(i).getDegree() == secondPoly.get(j).getDegree()) {
-                    float sum = firstPoly.get(i).getCoefficient() + secondPoly.get(j).getCoefficient();
-                    finalPoly.add(new Term(sum, i));
+        float sum;
+        for (int i = 0 ; i <= firstPolynomial.size() - 1; i++) {
+            Term firstPoly = firstPolynomial.get(i);
+            boolean matchFound = false;
+            for (int j = 0; j <= secondPolynomial.size() - 1 ; j++) {
+                Term secondPoly = secondPolynomial.get(j);
+                if (firstPoly.getDegree() == secondPoly.getDegree()) {
+                    sum = firstPoly.getCoefficient() + secondPoly.getCoefficient();
+                    sumPolynomial.add(new Term(sum, firstPoly.getDegree()));
+                    matchFound = true;
+                    break;
                 }
             }
-//            if (firstPoly.get(i).getDegree() == secondPoly.get(i).getDegree()) {
-//                float sum = firstPoly.get(i).getCoefficient() + secondPoly.get(i).getCoefficient();
-//                finalPoly.add(new Term(sum, i));
-//            } else {
-//                finalPoly.add(new Term());
-//            }
 
+            if (!matchFound) sumPolynomial.add(firstPoly);
         }
-        System.out.print("The Sum of the two polynomials is " + printPolynomial(finalPoly) + "\n");
+        System.out.println("The Sum of " + printPolynomial(firstPolynomial) + " and " +
+                printPolynomial(secondPolynomial)  + " is " + printPolynomial(sumPolynomial));
+        System.out.print("Press ENTER to continue...");
+        input.nextLine();
+        System.out.println();
+
     }
 
     public void polynomialSubtraction() {
@@ -65,14 +79,13 @@ public class PolynomialArithmetic {
 
     }
 
-
     public LinkedList<Term> construct() {
         LinkedList<Term> Polynomial = new LinkedList<>();
-        byte degrees = setDegreesOfPolynomial();
+        byte totalTerms = setTotalTerms();
 
-        for (int i = degrees; i >= 0; i--) {
-            System.out.print("Enter the numerical coefficient of the term with degree " + i + ": ");
-            float num = validateNumCoefficient();
+        for (int i = totalTerms; i >= 0; i--) {
+            System.out.print("Enter the Coefficient of the Term with Degree " + i + ": ");
+            float num = validateCoefficient();
             if (num != 0) {
                 Polynomial.add(new Term(num, i));
             }
@@ -82,7 +95,7 @@ public class PolynomialArithmetic {
         return Polynomial;
     }
 
-    public float validateNumCoefficient() {
+    public float validateCoefficient() {
         float number = 0;
         boolean flag = false;
 
@@ -97,20 +110,22 @@ public class PolynomialArithmetic {
         return number;
     }
 
-    public byte setDegreesOfPolynomial() {
-        System.out.print("How many degree are there in the polynomial: ");
-        byte degrees = 0;
+    public byte setTotalTerms() {
+        System.out.print("How many terms will there be in the polynomial: ");
+        byte totalTerms = 0;
         boolean flag = false;
 
         while (!flag) {
             try {
-                degrees = Byte.parseByte(input.nextLine());
-                flag = true;
-            } catch (NumberFormatException e) {
-                System.out.print("Invalid Input. Please enter a number: ");
+                totalTerms = Byte.parseByte(input.nextLine());
+                if (totalTerms < 0) System.out.print("Number must not be negative. Enter again: ");
+                else if (totalTerms > 10) System.out.print("Total terms is limited to 10 only. Enter a lower value: ");
+                else flag = true;
+            } catch (NumberFormatException | InputMismatchException e) {
+                System.out.print("Invalid Input. Please enter a Integer: ");
             }
         }
-        return degrees;
+        return totalTerms;
     }
 
     public String printPolynomial(LinkedList<Term> polynomial) {
@@ -182,23 +197,26 @@ public class PolynomialArithmetic {
 //            if (isLast) results.deleteCharAt(results.length() - 1);
 //        }
 
+
         for (Term term : polynomial) {
             boolean isFirst = term.getDegree() == polynomial.getFirst().getDegree();
             boolean isLast = term.getDegree() == polynomial.getLast().getDegree();
             boolean impliedCoefficients = term.getCoefficient() == 1 || term.getCoefficient() == 0 || term.getCoefficient() == -1;
             boolean impliedPowers = term.getDegree() <= 1;
 
-//            if (isFirst) results.append((term.getCoefficient()) >= 0 ? " - " : "");
-
             if (term.getCoefficient() < 0) {
-                results.append(" - ");
+                if (isFirst) results.append("- ");
+                else results.append(" - ");
+
                 if (impliedCoefficients) {
                     results.append(term.getLiteral());
                 } else {
                     results.append(Math.abs(term.getCoefficient())).append(term.getLiteral());
                 }
             } else if (impliedCoefficients) {
-                results.append(term.getLiteral());
+                if (isLast) results.append(" + ").append(term.getCoefficient());
+                else if (!isFirst) results.append(" + ").append(term.getLiteral());
+                else results.append(term.getLiteral());
             } else {
                 if(!isFirst) results.append(" + ");
                 results.append(term.getCoefficient()).append(term.getLiteral());
@@ -213,4 +231,5 @@ public class PolynomialArithmetic {
         }
         return results.toString();
     }
+
 }
